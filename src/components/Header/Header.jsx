00713144c90bf04dc0bb99e6971e0bbe6a0ca136
ChristopheRibault <c.ribault.dev@gmail.com>
2021-09-23 16:45:51 +0200

    feat: implement styled component instead of css files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,49 @@
 import { Link } from 'react-router-dom';
-import './Header.css';
+import styled from 'styled-components';
 
 import Logo from '../../assets/logo/logo_kasa.svg'
 
+const HeaderWrapper = styled.header`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1440px;
+  padding: 2rem;
+  @media (max-width: 768px) {
+    padding: .5rem;
+  }
+`
+const StyledLogo = styled.img`
+  height: 68px;
+  @media (max-width: 768px) {
+    height: 47px;
+  }
+`
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #FF6060;
+  font-size: 1.5em;
+  margin-left: 2em;
+  @media (max-width: 768px) {
+    text-transform: uppercase;
+    font-size: .8em;
+    margin-left: .8em;
+  }
+`
+
 function Header() {
   return (
-    <header className="Header">
+    <HeaderWrapper className="Header">
       <Link to='/'>
-        <img src={Logo} alt="logo kasa" />
+        <StyledLogo src={Logo} alt="logo kasa" />
       </Link>
       <nav>
-        <Link to='/'>Accueil</Link>
-        <Link to='/about'>A propos</Link>
+        <StyledLink to='/'>Accueil</StyledLink>
+        <StyledLink to='/about'>A propos</StyledLink>
       </nav>
-    </header>
+    </HeaderWrapper>
   );
 }
 
